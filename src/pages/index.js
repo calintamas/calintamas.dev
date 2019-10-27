8d0bbd9b8379e2220d9e0dc1c5d7c742bd683b40
Calin Tamas <calinntamas@gmail.com>
2019-10-27 23:52:14 +0200

    rm log

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,6 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
         {posts.map(({ node }) => {
-          console.log('node', node);
           const title = node.frontmatter.title || node.fields.slug
           return (
             <article key={node.fields.slug}>
